ad6855059e38451cfd9ed32c34c2b063da876717
Thomas Jessop <tljessop@hotmail.com>
2021-04-16 12:44:49 -0700

    Sending email with nodemailer

--- index.js
+++ index.js
@@ -53,9 +53,8 @@ app.get('/', (request, responce)=>{
 app.post('/send', (request, response) =>{
     const data = request.body;
 
-    console.log(data);
     saveData(data.firstNameS,data.lastNameS,data.companyS, data.email, data.cityS, data.stateS, data.firstNameR, data.lastNameR,data.companyR,data.cityR,data.stateR);
-
+    sendEmail(data.email);
     if (data.stateS == data.stateR) {
         response.render('ansYesCA');
     } else {
@@ -84,3 +83,74 @@ function saveData(firstNameS,lastNameS,companyS, email, cityS, stateS, firstName
         }
     });
 }//sendData
+
+function sendEmail(recp){
+    //Send email
+   
+    const output=`
+    <html><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                    
+                    
+                    <style>
+                        .forward-text-show {
+                            display: block !important;
+                            color: #222222 !important;
+                            text-align: center!important; 
+                            font-size: 11px!important;
+                        }
+                        .forward-link-show {
+                            display : inline !important;
+                            color: #2160d2 !important;
+                            text-decoration: underline !important;
+                            cursor: none !important;
+                        }
+                    </style>
+                
+                </head><body style="margin: 0 !important; padding: 0 !important;">
+    <table border="0" width="100%" cellspacing="0" cellpadding="0" bgcolor="#f7f9fc">
+    <tbody>
+    <tr>
+    <td height="30">
+    <p><span style="font-size: 18pt;">Thank You for your interest in shipping
+    Cannabis. We will keep you posted as the rapidly changing Interstate and
+    International Cannabis regulations allow us to serve you.</span></p>
+    <p><span style="font-size: 24pt;">CanU.Us&nbsp;</span></p>
+    <p><span style="font-size: 24pt;">CanU.World</span></p>
+    </td>
+    </tr>
+    <tr>
+    <td align="center"></td>
+    </tr>
+    <tr>
+    <td height="30"></td>
+    </tr>
+    </tbody>
+    </table>
+    </body></html>
+`;
+    const transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        secure: false, 
+        auth: {
+        user: 'testingnm040821@outlook.com',
+        pass: 'Password541'
+     }
+    });
+
+    const options = {
+        from: 'testingnm040821@outlook.com',
+        to: recp,
+        subject: 'CanU - Interstate Cannabis',
+        html: output
+    };
+
+    transporter.sendMail(options, (error, info) =>{
+        if (error){
+            return console.log(error);
+        }
+    
+        console.log('sent!!!');
+    });
+
+}//sendEmail
